f9e5c10cf59857879817593ea85de1bf61bf9554
Mahad <maqbom3@mcmaster.ca>
2025-01-04 18:36:29 +0300

    Created a proper game system.

--- script.js
+++ script.js
@@ -24,8 +24,10 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return "You Tied.";
     } else if ((humanChoice === "rock" && computerChoice == "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
         return "You Win! " + humanChoice + " beats " + computerChoice + ".";
     } else if ((computerChoice === "rock" && humanChoice == "scissors") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissors" && humanChoice === "paper")) {
+        computerScore++;
         return "You Lose. " + humanChoice + " does not beat " + computerChoice + ".";
     } else {
         return "Invalid user input.";
@@ -33,8 +35,45 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-console.log(playRound(humanSelection, computerSelection));
+    function playRound() {
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice().toLowerCase();
+
+        if (humanChoice === computerChoice) {
+            console.log("You Tied.");
+        } else if ((humanChoice === "rock" && computerChoice == "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+            humanScore++;
+            console.log("You Win! " + humanChoice + " beats " + computerChoice + ".");
+        } else if ((computerChoice === "rock" && humanChoice == "scissors") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissors" && humanChoice === "paper")) {
+            computerScore++;
+            console.log("You Lose. " + humanChoice + " does not beat " + computerChoice + ".");
+        } else {
+            console.log("Invalid user input.");
+        }
+
+        return;
+
+    }
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
+    if (humanScore > computerScore) {
+        console.log("Final Result: You have Won!");;
+    } else if (computerScore > humanScore) {
+        console.log("Final Result: You have Lost.");
+    } else {
+        console.log("Final Result: You Tied.");
+    }
+} 
+
+playGame();
+
 
